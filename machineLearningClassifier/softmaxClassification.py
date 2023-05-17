a5028ae6a2559d6f3a78c4d3d8ffe00c20c8d603
Joanne <joanne.zh2015@gmail.com>
2023-05-17 13:47:22 +0800

    rename a file

---
+++ machineLearningClassifier/softmaxClassification.py
@@ -0,0 +1,206 @@
+# Import the libraries we need for this lab
+
+# Using the following line code to install the torchvision library
+# !conda install -y torchvision
+
+# PyTorch Library
+import torch 
+# PyTorch Neural Network
+import torch.nn as nn
+# Allows us to transform data
+import torchvision.transforms as transforms
+# Allows us to get the digit dataset
+import torchvision.datasets as dsets
+# Creating graphs
+import matplotlib.pylab as plt
+# Allows us to use arrays to manipulate and store data
+import numpy as np
+import helperFunc as helper
+
+# make some data
+# Create and print the training dataset
+train_dataset = dsets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+print("Print the training dataset:\n ", train_dataset)
+
+# load the testing dataset and convert it to a tensor
+# Create and print the validation dataset
+validation_dataset = dsets.MNIST(root='./data', download=True, transform=transforms.ToTensor())
+print("Print the validation dataset:\n ", validation_dataset)
+
+# Print the first image and label
+print("First Image and Label", helper.show_data(train_dataset[0]))
+
+# Print the label of the fourth element
+print("The label: ", train_dataset[3][1])
+# The label: tensor(1)
+
+# Plot the image, the fourth samplt
+print("The image: ", helper.how_data(train_dataset[3]))
+
+# Plot the image, the third dample
+helper.show_data(train_dataset[2])
+
+## build a softmax classifier
+## the extension of logistic regression
+## softmax classifier is used for multi-class classification
+
+# Print the shape of the training dataset
+# requires vector inputs, shape is 28x28
+print(train_dataset[0][0].shape)
+
+# Set input size and output size
+input_dim = 28 * 28
+output_dim = 10
+
+## define the softmax classifier, criterion function, optimizer and train the model
+
+# Create the model
+# Input dim is 28*28 which is the image converted to a tensor
+# Output dim is 10 because there are 10 possible digits the image can be
+model = helper.SoftMax(input_dim, output_dim)
+print("Print the model:\n ", model)
+
+# Print the parameters
+print('W: ',list(model.parameters())[0].size())
+print('b: ',list(model.parameters())[1].size())
+
+# Plot the model parameters for each class
+# Since the model has not been trained yet the parameters look random
+helper.PlotParameters(model)
+
+## make a prediction
+# First we get the X value of the first image
+X = train_dataset[0][0]
+# We can see the shape is 1 by 28 by 28, we need it to be flattened to 1 by 28 * 28 (784)
+print(X.shape)
+X = X.view(-1, 28*28)
+print(X.shape)
+# Now we can make a prediction, each class has a value, and the higher it is the more confident the model is that it is that digit
+model(X)
+
+## using validation data to modify learning_rate and find the best division
+
+# Define the learning rate, optimizer, criterion, and data loader
+learning_rate = 0.1
+# The optimizer will updates the model parameters using the learning rate
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+# The criterion will measure the loss between the prediction and actual label values
+# This is where the SoftMax occurs, it is built into the Criterion Cross Entropy Loss
+criterion = nn.CrossEntropyLoss()
+# Created a training data loader so we can set the batch size
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100)
+# Created a validation data loader so we can set the batch size
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=5000)
+
+model_output = model(X)
+actual = torch.tensor([train_dataset[0][1]])
+
+helper.show_data(train_dataset[0])
+print("Output: ", model_output)
+print("Actual:", actual)
+
+# criterion will take these values and return a loss
+criterion(model_output, actual)
+
+softmax = nn.Softmax(dim=1)
+probability = softmax(model_output)
+print(probability)
+
+-1*torch.log(probability[0][actual])
+
+
+## train the model
+# Number of times we train our model useing the training data
+n_epochs = 10
+# Lists to keep track of loss and accuracy
+loss_list = []
+accuracy_list = []
+# Size of the validation data
+N_test = len(validation_dataset)
+
+# Function to train the model based on number of epochs
+def train_model(n_epochs):
+    # Loops n_epochs times
+    for epoch in range(n_epochs):
+        # For each batch in the train loader
+        for x, y in train_loader:
+            # Resets the calculated gradient value, this must be done each time as it accumulates if we do not reset
+            optimizer.zero_grad()
+            # Makes a prediction based on the image tensor
+            z = model(x.view(-1, 28 * 28))
+            # Calculates loss between the model output and actual class
+            loss = criterion(z, y)
+            # Calculates the gradient value with respect to each weight and bias
+            loss.backward()
+            # Updates the weight and bias according to calculated gradient value
+            optimizer.step()
+        
+        # Each epoch we check how the model performs with data it has not seen which is the validation data, we are not training here
+        correct = 0
+        # For each batch in the validation loader
+        for x_test, y_test in validation_loader:
+            # Makes prediction based on image tensor
+            z = model(x_test.view(-1, 28 * 28))
+            # Finds the class with the higest output
+            _, yhat = torch.max(z.data, 1)
+            # Checks if the prediction matches the actual class and increments correct if it does
+            correct += (yhat == y_test).sum().item()
+        # Calculates the accuracy by dividing correct by size of validation dataset
+        accuracy = correct / N_test
+        # Keeps track loss
+        loss_list.append(loss.data)
+        # Keeps track of the accuracy
+        accuracy_list.append(accuracy)
+
+# Function call
+train_model(n_epochs)
+
+# analyze results
+# Plot the loss and accuracy
+fig, ax1 = plt.subplots()
+color = 'tab:red'
+ax1.plot(loss_list,color=color)
+ax1.set_xlabel('epoch',color=color)
+ax1.set_ylabel('total loss',color=color)
+ax1.tick_params(axis='y', color=color)
+    
+ax2 = ax1.twinx()  
+color = 'tab:blue'
+ax2.set_ylabel('accuracy', color=color)  
+ax2.plot( accuracy_list, color=color)
+ax2.tick_params(axis='y', color=color)
+fig.tight_layout()
+
+# Plot the parameters
+
+helper.PlotParameters(model)
+
+# Plot the misclassified samples
+Softmax_fn=nn.Softmax(dim=-1)
+count = 0
+for x, y in validation_dataset:
+    z = model(x.reshape(-1, 28 * 28))
+    _, yhat = torch.max(z, 1)
+    if yhat != y:
+        helper.show_data((x, y))
+        plt.show()
+        print("yhat:", yhat)
+        print("probability of class ", torch.max(Softmax_fn(z)).item())
+        count += 1
+    if count >= 5:
+        break       
+
+# Plot the classified samples
+Softmax_fn=nn.Softmax(dim=-1)
+count = 0
+for x, y in validation_dataset:
+    z = model(x.reshape(-1, 28 * 28))
+    _, yhat = torch.max(z, 1)
+    if yhat == y:
+        helper.show_data((x, y))
+        plt.show()
+        print("yhat:", yhat)
+        print("probability of class ", torch.max(Softmax_fn(z)).item())
+        count += 1
+    if count >= 5:
+        break  
